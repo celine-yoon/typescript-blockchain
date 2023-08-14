38c8801ab047f1d9411089495edceef7b68fc068
Celine Yoon <celine.sy.yoon@gmail.com>
2023-08-14 13:14:13 +0900

    Add: Creating cryto

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,55 @@
-// import * as crypto from "crypto"
+import crypto from "crypto";
 
-// interface BlockShape {
-//     hash: string,
-//     prevHash: string,
-//     height: number,
-//     data: string
-// }
+interface BlockShape {
+  hash: string;
+  prevHash: string;
+  height: number;
+  data: string;
+}
 
-// class Block implements BlockShape {
-//     // public hash: string,
-//     // constructor (
-//     //     public prevHash: string,
-//     //     public height: number,
-//     //     public data: string
-//     // )
-//     // {
-//     //     this.hash = Block.calculateHash(prevHash, height, data)
-//     // }
-//     // static calculateHash(prevHash:string, height:number, data:string){
-//     //     const toHash=`${prevHash}${height}${data}`;
-//     // }
-// }
+class Block implements BlockShape {
+  public hash: string;
+  constructor(
+    public prevHash: string,
+    public height: number,
+    public data: string
+  ) {
+    this.hash = Block.calculateHash(prevHash, height, data);
+  }
+  static calculateHash(prevHash: string, height: number, data: string) {
+    const toHash = `${prevHash}${height}${data}`;
+    return crypto.createHash("sha256").update(toHash).digest("hex");
+    // sha256 stands for Secure Hash Algorithm 256-bit
+  }
+}
+
+class Blockchain {
+  private blocks: Block[];
+  constructor() {
+    this.blocks = [];
+  }
+  private getPrevHash() {
+    if (this.blocks.length === 0) return "";
+    return this.blocks[this.blocks.length - 1].hash;
+  }
+  public addBlock(data: string) {
+    const newBlock = new Block(
+      this.getPrevHash(),
+      this.blocks.length + 1,
+      data
+    );
+    this.blocks.push(newBlock);
+  }
+  public getBlocks() {
+    return [...this.blocks];
+  }
+}
+
+const blockchain = new Blockchain();
+
+blockchain.addBlock("First chain");
+blockchain.addBlock("Second chain");
+blockchain.addBlock("Third chain");
+blockchain.addBlock("Fourth chain");
+
+console.log(blockchain.getBlocks());
